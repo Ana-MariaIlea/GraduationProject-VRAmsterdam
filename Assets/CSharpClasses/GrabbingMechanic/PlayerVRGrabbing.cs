b6c0fb4531c56972bcb15671d43b530276d41543
Ana-Maria Ilea <62722821+Ana-MariaIlea@users.noreply.github.com>
2023-03-24 11:34:22 +0100

    Grabbing Mechanic code

--- Assets/CSharpClasses/GrabbingMechanic/PlayerVRGrabbing.cs
+++ Assets/CSharpClasses/GrabbingMechanic/PlayerVRGrabbing.cs
@@ -1,18 +1,109 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerVRGrabbing : MonoBehaviour
 {
+    public enum ControllerType
+    {
+        Left,
+        Right
+    }
+
+    [SerializeField] ControllerType controllerType;
+    private PlayerInputActions controls;
+
+    private GrabbableItem grabbable = null;
+
+    void Awake()
+    {
+        controls = new PlayerInputActions();
+    }
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        BindInputActions();
+    }
+
+    private void OnDestroy()
+    {
+        UnBindInputActions();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Grab")
+        {
+            grabbable = other.GetComponent<GrabbableItem>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Grab")
+        {
+            grabbable = null;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void BindInputActions()
+    {
+        if (controllerType == ControllerType.Left)
+        {
+            controls.PlayerPart1.GrabbingLeft.performed += GrabItem;
+            controls.PlayerPart1.GrabbingLeft.canceled += ResealseItem;
+        }
+        else
+        {
+            controls.PlayerPart1.GrabbingLeft.performed += GrabItem;
+            controls.PlayerPart1.GrabbingLeft.canceled += ResealseItem;
+        }
+
+    }
+
+    void UnBindInputActions()
+    {
+        if (controllerType == ControllerType.Left)
+        {
+            controls.PlayerPart1.GrabbingLeft.performed -= GrabItem;
+            controls.PlayerPart1.GrabbingLeft.canceled -= ResealseItem;
+        }
+        else
+        {
+            controls.PlayerPart1.GrabbingLeft.performed -= GrabItem;
+            controls.PlayerPart1.GrabbingLeft.canceled -= ResealseItem;
+        }
+    }
+
+    void GrabItem(InputAction.CallbackContext ctx)
+    {
+        if(grabbable != null)
+        {
+            grabbable.gameObject.transform.SetParent(this.gameObject.transform);
+        }
+    }
+
+    void ResealseItem(InputAction.CallbackContext ctx)
+    {
+        if (grabbable != null)
+        {
+            grabbable.gameObject.transform.SetParent(null);
+            grabbable = null;
+        }
     }
 }
