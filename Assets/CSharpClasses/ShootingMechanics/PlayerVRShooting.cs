64f75e2420816c928ea7c5cd58cc6fac46e0c64b
Ana-Maria Ilea <62722821+Ana-MariaIlea@users.noreply.github.com>
2023-03-27 12:31:55 +0200

    Shooting stream mechanic

--- Assets/CSharpClasses/ShootingMechanics/PlayerVRShooting.cs
+++ Assets/CSharpClasses/ShootingMechanics/PlayerVRShooting.cs
@@ -10,9 +10,19 @@ public class PlayerVRShooting : MonoBehaviour
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] Vector3 projectileOffset;
     [SerializeField] float projectileShootCooldown = 1;
+    [SerializeField] ParticleSystem streamObjectLeft;
+    [SerializeField] ParticleSystem streamObjectRight;
+    [SerializeField] float streamShootTime = 5;
+    [SerializeField] float streamShootCooldown = 3;
+
     private PlayerInputActions controls;
-    private Coroutine shootingProjectilesLeft = null;
-    private Coroutine shootingProjectilesRight = null;
+
+    private Coroutine shootingStreamLeft = null;
+    private Coroutine shootingStreamRight = null;
+
+    private Coroutine shootingStreamLeftCooldown = null;
+    private Coroutine shootingStreamRightCooldown = null;
+
     void Awake()
     {
         controls = new PlayerInputActions();
@@ -38,8 +48,14 @@ public class PlayerVRShooting : MonoBehaviour
 
     void BindInputActions()
     {
-        controls.PlayerPart2.ShootingLeft.performed += ShootProjectileLeftProxi;
-        controls.PlayerPart2.ShootingRight.performed += ShootProjectileRightProxi;
+        //controls.PlayerPart2.ShootingLeft.performed += ShootProjectileLeftProxi;
+        //controls.PlayerPart2.ShootingRight.performed += ShootProjectileRightProxi;
+
+        controls.PlayerPart2.ShootingLeft.performed += ShootStreamLeftProxi;
+        controls.PlayerPart2.ShootingRight.performed += ShootStreamRightProxi;
+
+        controls.PlayerPart2.ShootingLeft.canceled += StopShootStreamLeftProxi;
+        controls.PlayerPart2.ShootingRight.canceled += StopShootStreamRightProxi;
     }
 
     void UnBindInputActions()
@@ -58,7 +74,7 @@ public class PlayerVRShooting : MonoBehaviour
     private IEnumerator ShootProjectile(ControllerType controller)
     {
         Vector3 projectilePosition = transform.position;
-        Quaternion projectileRotation = transform.rotation;   
+        Quaternion projectileRotation = transform.rotation;
         if (controller == ControllerType.Left)
         {
             projectilePosition = controllerLeft.position + projectileOffset;
@@ -83,6 +99,110 @@ public class PlayerVRShooting : MonoBehaviour
         else
         {
             controls.PlayerPart2.ShootingRight.performed += ShootProjectileRightProxi;
+        }
+    }
+
+    private void ShootStreamLeftProxi(InputAction.CallbackContext ctx)
+    {
+        if (shootingStreamLeft == null)
+        {
+            shootingStreamLeft = StartCoroutine(ShootSteam(ControllerType.Left));
+        }
+    }
+    private void ShootStreamRightProxi(InputAction.CallbackContext ctx)
+    {
+        if (shootingStreamRight == null)
+        {
+            shootingStreamRight = StartCoroutine(ShootSteam(ControllerType.Right));
+        }
+    }
+
+    private void StopShootStreamLeftProxi(InputAction.CallbackContext ctx)
+    {
+        if (shootingStreamLeftCooldown == null)
+        {
+            shootingStreamLeftCooldown = StartCoroutine(ShootSteamPartialCooldown(ControllerType.Left));
+        }
+    }
+    private void StopShootStreamRightProxi(InputAction.CallbackContext ctx)
+    {
+        if (shootingStreamRightCooldown == null)
+        {
+            shootingStreamRightCooldown = StartCoroutine(ShootSteamPartialCooldown(ControllerType.Right));
+        }
+    }
+
+    private IEnumerator ShootSteam(ControllerType controller)
+    {
+        if (controller == ControllerType.Left)
+        {
+            streamObjectLeft.Play();
+        }
+        else
+        {
+            streamObjectRight.Play();
+        }
+
+        yield return new WaitForSeconds(streamShootTime);
+
+        if (controller == ControllerType.Left)
+        {
+            streamObjectLeft.Stop();
+            controls.PlayerPart2.ShootingLeft.performed -= ShootStreamLeftProxi;
+            shootingStreamLeft = null;
+        }
+        else
+        {
+            streamObjectRight.Stop();
+            controls.PlayerPart2.ShootingRight.performed -= ShootStreamRightProxi;
+            shootingStreamRight = null;
+        }
+    }
+
+    private IEnumerator ShootSteamPartialCooldown(ControllerType controller)
+    {
+        if (controller == ControllerType.Left)
+        {
+            if (shootingStreamLeft != null)
+            {
+                StopCoroutine(shootingStreamLeft);
+                streamObjectLeft.Stop();
+                controls.PlayerPart2.ShootingLeft.performed -= ShootStreamLeftProxi;
+            }
+
+            controls.PlayerPart2.ShootingLeft.canceled -= StopShootStreamLeftProxi;
+        }
+        else
+        {
+            if (shootingStreamRight != null)
+            {
+                streamObjectRight.Stop();
+                StopCoroutine(shootingStreamRight);
+                controls.PlayerPart2.ShootingRight.performed -= ShootStreamRightProxi;
+            }
+
+            controls.PlayerPart2.ShootingRight.canceled -= StopShootStreamRightProxi;
+        }
+        // add formula for partial stream cooldown
+        yield return new WaitForSeconds(streamShootCooldown);
+
+        if (controller == ControllerType.Left)
+        {
+            shootingStreamLeft = null;
+            shootingStreamLeftCooldown = null;
+
+            controls.PlayerPart2.ShootingLeft.performed += ShootStreamLeftProxi;
+
+            controls.PlayerPart2.ShootingLeft.canceled += StopShootStreamLeftProxi;
+        }
+        else
+        {
+            shootingStreamRight = null;
+            shootingStreamRightCooldown = null;
+
+            controls.PlayerPart2.ShootingRight.performed += ShootStreamRightProxi;
+
+            controls.PlayerPart2.ShootingRight.canceled += StopShootStreamRightProxi;
         }
     }
 
