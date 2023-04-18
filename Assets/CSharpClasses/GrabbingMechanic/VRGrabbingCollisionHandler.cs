623c5c0d10f2c9d6997b84a0573e49190d0c04d3
0MADXX <marcosrane@gmail.com>
2023-04-18 15:31:38 +0200

    Added Script for Haptics

--- Assets/CSharpClasses/GrabbingMechanic/VRGrabbingCollisionHandler.cs
+++ Assets/CSharpClasses/GrabbingMechanic/VRGrabbingCollisionHandler.cs
@@ -6,11 +6,14 @@ public class VRGrabbingCollisionHandler : MonoBehaviour
 {
     [SerializeField] private PlayerVRGrabbing grabbing;
 
+    OculusHaptics oculusHaptics;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Grab")
         {
             grabbing.TriggerEnterGrab(other);
+            //oculusHaptics.Vibrate(OculusHaptics.VibrationForce.Medium);
         }
         else if (other.tag == "GrabDestination")
         {
