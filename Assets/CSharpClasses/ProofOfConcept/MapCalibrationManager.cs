a056de9b6f458cbcd8d2b3d1a745efedc4625443
Kristýna Pavlátová <krpavlatova@gmail.com>
2023-05-10 16:26:19 +0200

    Calibration tutorial UI

---
+++ Assets/CSharpClasses/ProofOfConcept/MapCalibrationManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class manages UI and functionality for map calibration before the start of the game.
+/// The map calibration happens through adjusting the player's camera transform (OVRCameraRig).
+/// </summary>
+public class MapCalibrationManager : MonoBehaviour
+{
+    public List<GameObject> CalibrationUis;
+
+}
