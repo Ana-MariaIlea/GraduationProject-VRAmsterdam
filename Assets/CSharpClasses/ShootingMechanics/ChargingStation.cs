e4390f24d1b63e0be85e7e8c18ad042faee33d13
Ana-Maria Ilea <62722821+Ana-MariaIlea@users.noreply.github.com>
2023-06-09 11:15:47 +0200

    Charging station reset

--- Assets/CSharpClasses/ShootingMechanics/ChargingStation.cs
+++ Assets/CSharpClasses/ShootingMechanics/ChargingStation.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class ChargingStation : MonoBehaviour
+public class ChargingStation : NetworkBehaviour
 {
     [SerializeField] private CreatureType creatureType;
 
@@ -13,5 +17,43 @@ public class ChargingStation : MonoBehaviour
             //Some other code
             return creatureType;
         }
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer)
+        {
+            base.OnNetworkSpawn();
+            if (PlayerStateManager.Singleton)
+            {
+                PlayerStateManager.Singleton.endingStartServer.AddListener(GameEnd);
+            }
+            else
+            {
+                Debug.LogError("No PlayerStateManager in the scene");
+            }
+        }
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (IsServer)
+        {
+            base.OnNetworkDespawn();
+            if (PlayerStateManager.Singleton)
+            {
+                PlayerStateManager.Singleton.endingStartServer.RemoveListener(GameEnd);
+            }
+            else
+            {
+                Debug.LogError("No PlayerStateManager in the scene");
+            }
+        }
+    }
+
+    private void GameEnd()
+    {
+        GetComponent<NetworkObject>().Despawn();
+        Destroy(gameObject);
     }
 }
